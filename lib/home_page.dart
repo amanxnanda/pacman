a5b149f3c005030d03c53c856b419838d6c30124
Aman Nanda <34944818+amannanda8@users.noreply.github.com>
2021-04-25 20:23:37 +0530

    complete

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -19,13 +19,94 @@ class _HomePageState extends State<HomePage> {
   int numberOfSquares = numberInRow * 17;
   int player = numberInRow * 15 + 1;
 
+  String direction = 'right';
+
+  List<int> food = [];
+
+  static bool reset = false;
+  int score = 0;
+
+  void resetGame() {
+    setState(() {
+      reset = true;
+      player = numberInRow * 15 + 1;
+      food.clear();
+      direction = 'right';
+      score = 0;
+    });
+
+    Future.delayed(Duration(seconds: 2), () {
+      reset = false;
+    });
+  }
+
   void startGame() {
-    Timer.periodic(Duration(milliseconds: 120), (timer) {
-      setState(() {
-        if (!barriers.contains(player + 1)) {
-          player++;
+    getFood();
+    Timer.periodic(Duration(milliseconds: 200), (timer) {
+      if (reset) {
+        timer.cancel();
+      }
+      if (food.contains(player)) {
+        food.remove(player);
+        score++;
+      }
+
+      if (timer.isActive) {
+        switch (direction) {
+          case 'left':
+            moveLeft();
+            break;
+          case 'right':
+            moveRight();
+            break;
+          case 'up':
+            moveUp();
+            break;
+          case 'down':
+            moveDown();
+            break;
         }
-      });
+      }
+    });
+  }
+
+  void getFood() {
+    for (var i = 0; i < numberOfSquares; i++) {
+      if (!barriers.contains(i)) {
+        food.add(i);
+      }
+    }
+  }
+
+  void moveLeft() {
+    setState(() {
+      if (!barriers.contains(player - 1)) {
+        player--;
+      }
+    });
+  }
+
+  void moveRight() {
+    setState(() {
+      if (!barriers.contains(player + 1)) {
+        player++;
+      }
+    });
+  }
+
+  void moveUp() {
+    setState(() {
+      if (!barriers.contains(player - numberInRow)) {
+        player -= numberInRow;
+      }
+    });
+  }
+
+  void moveDown() {
+    setState(() {
+      if (!barriers.contains(player + numberInRow)) {
+        player += numberInRow;
+      }
     });
   }
 
@@ -38,6 +119,22 @@ class _HomePageState extends State<HomePage> {
           Expanded(
             flex: 6,
             child: GestureDetector(
+              onVerticalDragUpdate: (details) {
+                if (details.delta.dy > 0) {
+                  direction = 'down';
+                } else if (details.delta.dy < 0) {
+                  direction = 'up';
+                }
+                print(direction);
+              },
+              onHorizontalDragUpdate: (details) {
+                if (details.delta.dx > 0) {
+                  direction = 'right';
+                } else if (details.delta.dx < 0) {
+                  direction = 'left';
+                }
+                print(direction);
+              },
               child: Container(
                 child: GridView.builder(
                   itemCount: numberOfSquares,
@@ -51,12 +148,28 @@ class _HomePageState extends State<HomePage> {
                         innerColor: Colors.blue[700],
                       );
                     } else if (index == player) {
-                      return MyPlayer();
+                      switch (direction) {
+                        case 'left':
+                          return RotatedBox(quarterTurns: 2, child: MyPlayer());
+                          break;
+                        case 'right':
+                          return MyPlayer();
+                          break;
+                        case 'up':
+                          return RotatedBox(quarterTurns: 3, child: MyPlayer());
+                          break;
+                        case 'down':
+                          return RotatedBox(quarterTurns: 1, child: MyPlayer());
+                          break;
+                      }
                     }
-
-                    return MyPath(
-                      innerColor: Colors.yellow,
-                    );
+                    if (food.contains(index)) {
+                      return MyPath(
+                        innerColor: Colors.yellow,
+                      );
+                    } else {
+                      return Container();
+                    }
                   },
                 ),
               ),
@@ -65,28 +178,46 @@ class _HomePageState extends State<HomePage> {
           Expanded(
             child: Container(
               // color: Colors.black,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Text(
-                    'Score: ',
+                    'SCORE: $score',
                     style: TextStyle(
                       color: Colors.white,
                       fontSize: 40,
                     ),
                   ),
-                  GestureDetector(
-                    onTap: startGame,
-                    child: Card(
-                      color: Colors.grey[800],
-                      child: Text(
-                        'P L A Y',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 40,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      GestureDetector(
+                        onTap: startGame,
+                        child: Card(
+                          color: Colors.grey[800],
+                          child: Text(
+                            'P L A Y',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 40,
+                            ),
+                          ),
                         ),
                       ),
-                    ),
+                      GestureDetector(
+                        onTap: resetGame,
+                        child: Card(
+                          color: Colors.grey[800],
+                          child: Text(
+                            'RESET',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 40,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
